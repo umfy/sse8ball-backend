77fb36ff28332153809b508a2c146d4428f29f63
umfy <39531680+umfy@users.noreply.github.com>
2022-04-13 17:34:22 +0200

    initial

---
+++ routes.ts
@@ -0,0 +1,57 @@
+import { cyan, green } from 'https://deno.land/std@0.131.0/fmt/colors.ts'
+import {
+  Status,
+  Router,
+  Context,
+  ServerSentEvent,
+  ServerSentEventTarget,
+} from 'https://deno.land/x/oak@v10.5.1/mod.ts'
+
+const router = new Router()
+
+let clients: ServerSentEventTarget[] = []
+
+router
+  .get('/', async (ctx) => {
+    await ctx.send({
+      root: `${Deno.cwd()}/views`,
+      path: 'sseServer_index.html',
+    })
+  })
+  // for any clients that request the `/sse` endpoint, we will send a message
+  // every 2 seconds.
+  .get('/sse', (ctx: Context) => {
+    ctx.assert(
+      ctx.request.accepts('text/event-stream'),
+      Status.UnsupportedMediaType
+    )
+    const connection = ctx.request.ip
+    const target = ctx.sendEvents()
+    clients.push(target)
+    console.log(`${green('SSE connection')} ${cyan(connection)}`)
+
+    target.addEventListener('close', () => {
+      console.log(`${green('SSE disconnect')} ${cyan(connection)}`)
+      clients = clients.filter((el) => el != target)
+      // if browser is terminated (x) there is no close event,
+      // so i double check in /trigger and remove unused clients
+    })
+  })
+
+  .post('/trigger', async (ctx: Context) => {
+    const { value } = await ctx.request.body().value
+    const evt = new ServerSentEvent('message', { value }, { id: 0 })
+
+    for (let i = 0; i < clients.length; i++) {
+      console.log(i, clients[i].closed)
+      if (clients[i].closed) {
+        clients.splice(i, 1)
+        i--
+        continue
+      }
+      // this causes uncaught errors, cannot trycatch it
+      clients[i].dispatchEvent(evt)
+    }
+  })
+
+export default router
---
+++ server.ts
@@ -0,0 +1,78 @@
+import {
+  bold,
+  cyan,
+  green,
+  yellow,
+} from 'https://deno.land/std@0.131.0/fmt/colors.ts'
+
+import {
+  Application,
+  Context,
+  isHttpError,
+  Status,
+} from 'https://deno.land/x/oak@v10.5.1/mod.ts'
+
+import router from './routes.ts'
+
+function notFound(ctx: Context) {
+  ctx.response.status = Status.NotFound
+  ctx.response.body = `<html><body><h1>404 - Not Found</h1><p>Path <code>${ctx.request.url}</code> not found.`
+}
+
+const app = new Application()
+
+// Logger
+app.use(async (context, next) => {
+  await next()
+  const rt = context.response.headers.get('X-Response-Time')
+  console.log(
+    `${green(context.request.method)} ${cyan(
+      context.request.url.pathname
+    )} - ${bold(String(rt))}`
+  )
+})
+
+// Response Time
+app.use(async (context, next) => {
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  context.response.headers.set('X-Response-Time', `${ms}ms`)
+})
+
+// Error handler
+app.use(async (context, next) => {
+  try {
+    await next()
+  } catch (err) {
+    if (isHttpError(err)) {
+      context.response.status = err.status
+      const { message, status, stack } = err
+      if (context.request.accepts('json')) {
+        context.response.body = { message, status, stack }
+        context.response.type = 'json'
+      } else {
+        context.response.body = `${status} ${message}\n\n${stack ?? ''}`
+        context.response.type = 'text/plain'
+      }
+    } else {
+      console.log(err)
+      throw err
+    }
+  }
+})
+
+// Use the router
+app.use(router.routes())
+app.use(router.allowedMethods())
+// A basic 404 page
+app.use(notFound)
+
+// better than console log
+app.addEventListener('listen', ({ hostname, port, serverType }) => {
+  console.log(bold('Start listening on ') + yellow(`${hostname}:${port}`))
+  console.log(bold('  using HTTP server: ' + yellow(serverType)))
+})
+
+await app.listen({ hostname: '127.0.0.1', port: 8000 })
+console.log(bold('Finished.'))
